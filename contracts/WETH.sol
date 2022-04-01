950e5e0db0519e94ad32e975690e61587297fe2a
Juan G. Molina <jg.molina92@hotmail.com>
2022-04-01 17:18:30 -0500

    Add WETH Smart Contract and some tests

--- contracts/WETH.sol
+++ contracts/WETH.sol
@@ -2,5 +2,22 @@ pragma solidity ^0.8.1;
 
 //solhint-disable no-empty-blocks
 contract WETH {
+  mapping(address => uint256) public balanceOf;
 
+  function deposit() external payable {
+    balanceOf[msg.sender] += msg.value;
+  }
+
+  function withdraw(uint256 value) external {
+    uint256 balance = balanceOf[msg.sender];
+    require(balance >= value, "WETH: burn amount exceeds balance");
+    balanceOf[msg.sender] = balance - value;
+
+    (bool success, ) = msg.sender.call{ value: value }("");
+    require(success, "WETH: ETH transfer failed");
+  }
+
+  receive() external payable {
+      balanceOf[msg.sender] += msg.value;
+  }
 }
